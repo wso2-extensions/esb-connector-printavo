03dcab91837d883b8d49fd46f91d9c72cd696995
ashalya <ashalya86@gmail.com>
2016-02-26 18:22:37 +0530

    connector version changed

--- src/test/java/org/wso2/carbon/connector/integration/test/printavo/PrintavoConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/printavo/PrintavoConnectorIntegrationTest.java
@@ -46,7 +46,7 @@ public class PrintavoConnectorIntegrationTest extends ConnectorIntegrationTestBa
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
         
-        init("printavo-connector-1.0.0");
+        init("printavo-connector-1.0.1-SNAPSHOT");
         
         esbRequestHeadersMap = new HashMap<String, String>();
         apiRequestHeadersMap = new HashMap<String, String>();
